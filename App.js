4ba2d72341e00c8053e20c7d4dce0f307c1f270b
Gilson Mendes <gilson.mendes00@hotmail.com>
2022-06-21 12:43:16 -0300

    first commit

---
+++ App.js
@@ -0,0 +1,51 @@
+
+
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+
+export default App => {
+
+  return(
+    <Text style= {styles.sectionContainer}>Olá! Estamos aqui!</Text>
+  )
+
+}
+
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+});
+
+
